7872c93fbead1e0147c114920a5f52ba18822e43
Dan Florian <dan@attacksimulator.com>
2025-07-11 20:00:16 +0300

    Minor fixes

--- src/index.js
+++ src/index.js
@@ -4,11 +4,4 @@ import Action from "./components/Action";
 
 const Components = { Action, Column, vTable };
 
-export default {
-  install(app) {
-    for (let key in Components) {
-      app.component(key, Components[key]);
-    }
-  },
-  ...Components
-};
+export default Components;
